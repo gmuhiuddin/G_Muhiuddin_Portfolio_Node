68bc649e325dbd016178a578087fe0ac02bc5790
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-05-07 16:00:53 +0500

    add port work

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 import db from './config/db.js';
 import routes from './routes/index.js';
 import { verifyApiAuth } from './middleWares/verifyToken.js';
 
 const app = express();
+
+dotenv.config();
 
 db.connection.once('open', () => {
     console.log("Db connected successfully");
@@ -18,10 +21,10 @@ app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
+const port = process.env.PORT || 3001;
+
 app.listen(3001, () => {
     console.log("Server started successfully");
 });
 
-// app.use('/:authstoken',verifyApiAuth, routes);
-
-app.use('/', routes);
+app.use('/:authstoken',verifyApiAuth, routes);
